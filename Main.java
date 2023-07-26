d08344c4d6c46916369bd29bf04177e1b4d5dc58
Alvin Alic <alvin.alic@gmail.com>
2023-07-25 21:23:40 -0500

    Add Instruction for user

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the arithmetic you would like to do");
+        System.out.println("Ex: '5+7' or '8*10'");
+        String math = input.nextLine();
+
     }
 }
