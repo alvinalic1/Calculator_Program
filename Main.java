8d2951f65fbd8e73c37a793b8562c7d863eb9e3c
Alvin Alic <alvin.alic@gmail.com>
2023-07-25 21:40:27 -0500

    Complete Calculator Program

--- Main.java
+++ Main.java
@@ -11,6 +11,27 @@ public class Main {
             int temp2 = input.nextInt();
             System.out.println(temp1+ " + "+temp2 + " = " + (temp1+temp2));
         }
+        else if(answer.equalsIgnoreCase("s"))
+        {
+            System.out.println("What two numbers would you like to subtract?");
+            int temp1 = input.nextInt();
+            int temp2 = input.nextInt();
+            System.out.println(temp1+ " - "+temp2 + " = " + (temp1-temp2));
+        }
+        else if(answer.equalsIgnoreCase("m"))
+        {
+            System.out.println("What two numbers would you like to multiply?");
+            int temp1 = input.nextInt();
+            int temp2 = input.nextInt();
+            System.out.println(temp1+ " * "+temp2 + " = " + (temp1*temp2));
+        }
+        else if(answer.equalsIgnoreCase("d"))
+        {
+            System.out.println("What two numbers would you like to divide?");
+            int temp1 = input.nextInt();
+            int temp2 = input.nextInt();
+            System.out.println(temp1+ " / "+temp2 + " = " + (float)(temp1/temp2));
+        }
 
     }
 }
